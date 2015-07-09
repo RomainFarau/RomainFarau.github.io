497f00067e2bc8aab451a49a7871b109fa2bf422
Romain Farau <romain.farau@gmail.com>
2015-07-09 16:02:00 +0200

    Ajout page amelioration desisgn en cours

--- cvFarauRomain/public/app/config.js
+++ cvFarauRomain/public/app/config.js
@@ -19,12 +19,55 @@ angular.module("cvModule",['ui.router','ui.bootstrap'])
 			}
 		})
 		.state('experience', {
+			url:'/',
 			views:{
 				'header': {
 						templateUrl: 'view/header.html'
 					},
 				'content': {
 					templateUrl: 'view/experience.html'
+				},
+				'footer':{
+						templateUrl: 'view/footer.html'
+				}
+			}
+		})
+		.state('formations', {
+			url:'/',
+			views:{
+				'header': {
+						templateUrl: 'view/header.html'
+					},
+				'content': {
+					templateUrl: 'view/formations.html'
+				},
+				'footer':{
+						templateUrl: 'view/footer.html'
+				}
+			}
+		})
+		.state('competences', {
+			url:'/',
+			views:{
+				'header': {
+						templateUrl: 'view/header.html'
+					},
+				'content': {
+					templateUrl: 'view/competences.html'
+				},
+				'footer':{
+						templateUrl: 'view/footer.html'
+				}
+			}
+		})
+		.state('loisirs', {
+			url:'/',
+			views:{
+				'header': {
+						templateUrl: 'view/header.html'
+					},
+				'content': {
+					templateUrl: 'view/loisirs.html'
 				},
 				'footer':{
 						templateUrl: 'view/footer.html'
